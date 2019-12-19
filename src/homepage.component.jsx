fac19001f537c1b3aa7f66d569b92decce89d1e2
unknown <mp@appxtechnologiex.com>
2019-12-19 14:28:26 +0530

    create home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './homepage.component.style.scss';
+
+const Homepage = () => (
+    <div className="image-box-outter">
+        <div className='image-box'>
+             <div className='image-box-text'>
+                 <h2 className='image-box-text-title'>1</h2>
+                 <p className='image-box-text-para'>Create / Find a Learning Path </p>
+            </div>
+        </div>
+
+        <div className='image-box'>
+             <div className='image-box-text'>
+                 <h2 className='image-box-text-title'>2</h2>
+                 <p className='image-box-text-para'>Create / Find a Learning Path </p>
+            </div>
+        </div>
+
+        <div className='image-box'>
+             <div className='image-box-text'>
+                 <h2 className='image-box-text-title'>3</h2>
+                 <p className='image-box-text-para'>Create / Find a Learning Path </p>
+            </div>
+        </div>
+
+        <div className='image-box'>
+             <div className='image-box-text'>
+                 <h2 className='image-box-text-title'>4</h2>
+                 <p className='image-box-text-para'>Create / Find a Learning Path </p>
+            </div>
+        </div>
+
+        <div className='image-box'>
+             <div className='image-box-text'>
+                 <h2 className='image-box-text-title'>5</h2>
+                 <p className='image-box-text-para'>Create / Find a Learning Path </p>
+            </div>
+        </div>
+
+        <div className='image-box'>
+             <div className='image-box-text'>
+                 <h2 className='image-box-text-title'>6</h2>
+                 <p className='image-box-text-para'>Create / Find a Learning Path </p>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage;
